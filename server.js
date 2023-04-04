ca554f17b59cdabe6e6155937030fe141a0ad5dc
Anthony Tepach <anthony.tepach@gmail.com>
2023-04-04 16:26:22 -0600

    separadno por modulos

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const { Pool } = require('pg');
-require('dotenv').config();
+const pool = require('./db');
+
 
 const app = express();
-
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
-
 
 pool.connect((err) => {
     if (err) {
